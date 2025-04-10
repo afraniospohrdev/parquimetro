d7fe8abd419184c51594c64cf1a1f6142e801ffa
Afranio <afraniorogerio@gmail.com>
2025-04-10 17:27:51 -0300

    parquimetro

--- script.js
+++ script.js
@@ -1,64 +1,36 @@
-var array = [];
+function calcular() {
 
-function calcular(){
-    var tempoPermanencia = $("#tempoPermanencia").val();
-    var totalAcumulado = 0;
+  this.valorPago = document.getElementById("valor").value;
+  this.tempo = 0;
+  this.valorPagar = 0;
+  this._valorMinimo = 1.0;
+  this._valorMáximo = 3.0;
+  this.troco = 0;
+  
+    
+  if (this.valorPago < this._valorMinimo) {
+      document.getElementById("result").innerHTML = "Valor insuficiente";
+      return;
+   }
 
+   if (this.valorPago >= 1 && this.valorPago < 1.75) {
+      this.valorPagar = 1;
+      this.tempo = 30;
+  } else if (this.valorPago >= 1.75 && this.valorPago < 3) {
+      this.valorPagar = 1.75;
+      this.tempo = 60;
+   } else if (this.valorPago >= 3.0) {
+      this.valorPagar = 3.0;
+      this.tempo = 120;
+   }
 
-var parquimetro = new Object();
-parquimetro['tempoPermanencia'] = tempoPermanencia;
-parquimetro['totalAcumulado'] = totalAcumulado;
-
-
-array.push(parquimetro);
-
-for (i = 0; i < array.length; i++){
-
-    var hora = $("#tempoPermanencia").val().split(":");
-
-
-    if (array[i].tempoPermanencia >= "01:00"){
-
-        if(minutos[1] == "00"){
-            totalAcumulado += 5 * minutos[0];
-        }
-        else if(minutos[1] != "00"){
-            totalAcumulado += (5 * minutos[0]) + 2.5;
-
-            console.log(minutos);
-
-        }
-
-    }else if (array[i].tempoPermanencia == "00:30"){
-
-        totalAcumulado += 1,00;
-    }
-    else if (array[i].tempoPermanencia == "01:00"){
-
-        totalAcumulado += 1,75;
-
-    }
-    else if (array[i].tempoPermanencia >= "02:00"){         
-
-        if(minutos[1] == "00"){ 
-            totalAcumulado += (minutos[0] * 2) * 2;
-
-            console.log(minutos);
-
-        }else if(minutos[1] != "00"){
-            totalAcumulado += (minutos[0] * 2 + 1) * 2;
-
-            console.log(minutos);
-
-        }
-
-    }
-//$("#total").val(totalAcumulado);
-//$("#tempoPermanencia").val("");
-//$("#tempoPermanencia").focus();
-
-}
-$("#total").val(totalAcumulado);
-$("#tempoPermanencia").val("");
-$("#tempoPermanencia").focus(); 
+  this.troco = this.valorPago - this.valorPagar;
+  
+  if (this.troco > 0) {
+  document.getElementById("tempo").value = this.tempo;
+  document.getElementById("result").innerHTML = `Você pagou o valor de R$ ${this.valorPagar} e tem direito a permanecer por ${this.tempo} minutos, com um troco de R$ ${this.troco}`;
+  } else {
+  document.getElementById("tempo").value = this.tempo;
+  document.getElementById("result").innerHTML = `Você pagou o valor de R$ ${this.valorPagar} e tem direito a permanecer por ${this.tempo} minutos`;
+  }
 }
